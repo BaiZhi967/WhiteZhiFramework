766721abb61cb4247a009365f744440557055cfa
BaiZhi967 <moycx@qq.com>
2024-03-02 01:22:45 +0800

    IOC容器和Architecture架构的接入

---
+++ Architecture/Architecture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace WhiteZhi
+{
+    /// <summary>
+    /// 架构
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Architecture<T> where T : Architecture<T>, new()
+    {
+        #region 单例 但仅在内部访问
+
+        private static T mArchitecture = null;
+
+        static void MakeSureArchitecture()
+        {
+            if (mArchitecture is null)
+            {
+                mArchitecture = new T();
+                mArchitecture.Init();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 注册模块的容器
+        /// </summary>
+        private IOCContainer mContainer = new IOCContainer();
+
+        /// <summary>
+        /// 留给子类自行注册模块
+        /// </summary>
+        protected abstract void Init();
+
+        /// <summary>
+        /// 注册模块的API
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <typeparam name="T"></typeparam>
+        public void Register<T>(T instance)
+        {
+            MakeSureArchitecture();//确保架构已经初始化
+            mArchitecture.mContainer.Register<T>(instance);
+        }
+
+        public static T Get<T>() where T : class
+        {
+            MakeSureArchitecture();//确保架构已经初始化
+            return mArchitecture.mContainer.Get<T>();
+        }
+    }
+}
---
+++ IOC/IOCContainer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhiteZhi
+{
+    /// <summary>
+    /// IOC容器
+    /// </summary>
+    public class IOCContainer
+    {
+        /// <summary>
+        /// 实例
+        /// </summary>
+        private Dictionary<Type, object> mInstances = new Dictionary<Type, object>();
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <typeparam name="T"></typeparam>
+        public void Register<T>(T instance)
+        {
+            var key = typeof(T);
+
+            if (mInstances.ContainsKey(key))
+            {
+                mInstances[key] = instance;
+            }
+            else
+            {
+                mInstances.Add(key, instance);
+            }
+        }
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Get<T>() where T : class
+        {
+            var key = typeof(T);
+
+            if (mInstances.TryGetValue(key, out var retInstance))
+            {
+                return retInstance as T;
+            }
+
+            return null;
+        }
+
+        public IEnumerable<T> GetInstancesByType<T>()
+        {
+            var type = typeof(T);
+            return mInstances.Values.Where(instance => type.IsInstanceOfType(instance)).Cast<T>();
+        }
+
+        public void Clear() => mInstances.Clear();
+    }
+}
