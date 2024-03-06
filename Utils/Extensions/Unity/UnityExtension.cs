597a2381e0808cb8dc769149fdf6ebea78ee47aa
BaiZhi967 <moycx@qq.com>
2024-03-06 18:54:42 +0800

    添加Unity相关扩展

---
+++ Utils/Extensions/Unity/UnityExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhiteZhi
+{
+    /// <summary>
+    /// Unity编辑器扩展
+    /// </summary>
+    public static partial class UnityExtension
+    {
+        
+
+        
+
+       
+
+        
+        
+        
+        
+        
+    }
+}
---
+++ Utils/Utils.Algorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhiteZhi
+{
+    public static partial class Utils
+    {
+        /// <summary>
+        /// 随机数相关函数
+        /// </summary>
+        public static class Algorithm
+        {
+            /// <summary>
+            /// 获得传入元素某个符合条件的所有对象
+            /// </summary>
+            public static T[] FindAll<T>(IList<T> array, Predicate<T> handler)
+            {
+                var dstArray = new T[array.Count];
+                int idx = 0;
+                for (int i = 0; i < array.Count; i++)
+                {
+                    if (handler(array[i]))
+                    {
+                        dstArray[idx] = array[i];
+                        idx++;
+                    }
+                }
+                Array.Resize(ref dstArray, idx);
+                return dstArray;
+            }
+        }
+    }
+}
